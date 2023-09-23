aec88d1284fa21b4be0f4c6f809332f0388b7858
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-09-23 10:43:10 -0300

    ✨ Funcionalidade de enviar emails

---
+++ src/Service/EmailSender.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Alura\Auction\Service;
+use Alura\Auction\Model\Auction;
+
+class EmailSender
+{
+    private const USER_EMAIL = "usuario@email.com";
+    public function notifyEndOfAuction(Auction $auction): void
+    {
+        $success = mail(
+            self::USER_EMAIL, 
+            "Leilão finalizado", 
+            "O leilão para {$auction->getDescription()} foi finalizado."
+        );
+
+        if (!$success) {
+            throw new \DomainException("Erro ao enviar email.");
+        }
+    }
+}
